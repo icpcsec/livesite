d6b2aef8904741979b5f5faabda213d531bed42f
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2016-05-09 03:47:45 +0900

    Enable wsgiprof

--- server/livesite/wsgi_main.py
+++ server/livesite/wsgi_main.py
@@ -2,14 +2,20 @@ import sys
 
 import bottle
 import gflags
+import wsgiprof
 
 from livesite import handler as handler_import_only
 from livesite import setup
 
 FLAGS = gflags.FLAGS
 
+gflags.DEFINE_bool('profile', False, 'Enable profiler.')
+
 app = bottle.default_app()
 
 assert __name__ == 'livesite.wsgi_main'
 
 setup.setup()
+
+if FLAGS.profile:
+  app = wsgiprof.ProfileMiddleware(app)
