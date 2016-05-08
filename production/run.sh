5fbcbfba1bbfa8b0c99341991d0868c36a065dda
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2016-05-09 03:49:31 +0900

    Clean up run.sh

--- production/run.sh
+++ production/run.sh
@@ -3,8 +3,11 @@
 exec uwsgi \
   --module=livesite.wsgi_main:app \
   --logto=/dev/null \
-  --logger=syslog:livesite \
-  --http-socket=8000 \
+  --logger=syslog:livesite-server \
+  --http-socket=:8000 \
+  --uwsgi-socket=:9000 \
+  --listen=10000 \
   --master \
-  --workers=$(nproc) \
+  --workers=$(( $(nproc) * 4 )) \
+  --threads=64 \
   "$@"
