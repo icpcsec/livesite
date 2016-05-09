c9d52fbc94481741f9a76d3f379273d2c555f013
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2016-05-09 22:17:49 +0900

    Tune uwsgi config

--- production/run.sh
+++ production/run.sh
@@ -2,12 +2,13 @@
 
 exec uwsgi \
   --module=livesite.wsgi_main:app \
+  --lazy-app \
   --logto=/dev/null \
   --logger=syslog:livesite-server \
   --http-socket=:8000 \
   --uwsgi-socket=:9000 \
   --listen=10000 \
   --master \
-  --workers=$(( $(nproc) * 4 )) \
+  --workers=$(( $(nproc) * 2 )) \
   --threads=64 \
   "$@"
