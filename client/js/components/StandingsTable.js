aee7e25e34736e8d8af4426cd5987226977d977e
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2016-05-14 03:02:05 +0900

    Standings: detailed/simple view

--- client/js/components/StandingsTable.js
+++ client/js/components/StandingsTable.js
@@ -11,7 +11,7 @@ const DEFAULT_TEAM = {
   members: [],
 };
 
-const StandingsLegendProblemCol = ({ problem: { label, title, color = 'black' } }) => {
+const LegendProblemCol = ({ problem: { label, title, color = 'black' } }) => {
   return (
     <th className="team-problem">
       <span title={title}>
@@ -24,27 +24,19 @@ const StandingsLegendProblemCol = ({ problem: { label, title, color = 'black' } 
   );
 };
 
-const StandingsLegendRow = ({ problems, detailed }) => {
-  const cols = [];
-  if (detailed) {
-    cols.push(...problems.map(
-      (problem) => <StandingsLegendProblemCol problem={problem} />));
-  } else {
-    cols.push(<th style={{ paddingLeft: '40px' }}>TODO: ここに何を表示するか考える</th>);
-  }
-  if (cols.length == 0) {
-    cols.push(<th />);
-  }
+const LegendRowSimple = ({ problems }) => {
   return (
     <li className="team-row legend">
       <table className="team-table">
         <tbody>
           <tr>
-            <th className="team-mark">{' '}</th>
+            <th className="team-mark"></th>
             <th className="team-rank">#</th>
-            <th className="team-name">チーム/大学</th>
-            <th className="team-score">スコア</th>
-            {cols}
+            <th className="team-name">チーム</th>
+            <th className="team-name">大学</th>
+            <th className="team-solved">正答数</th>
+            <th className="team-penalty">ペナルティ</th>
+            <th />
           </tr>
         </tbody>
       </table>
@@ -52,7 +44,40 @@ const StandingsLegendRow = ({ problems, detailed }) => {
   );
 };
 
-const StandingsProblemCol = ({ problem: { attempts, penalty, pendings, solved } }) => {
+const LegendRowDetailed = ({ problems }) => {
+  const problemCols = [];
+  if (problems.length > 0) {
+    problems.forEach((problem) => {
+      problemCols.push(<LegendProblemCol problem={problem} />);
+    });
+  } else {
+    problemCols.push(<th />);
+  }
+  return (
+    <li className="team-row legend">
+      <table className="team-table">
+        <tbody>
+          <tr>
+            <th className="team-mark"></th>
+            <th className="team-rank">#</th>
+            <th className="team-name">チーム/大学</th>
+            <th className="team-solved">正答数</th>
+            {problemCols}
+          </tr>
+        </tbody>
+      </table>
+    </li>
+  );
+};
+
+const TeamCol = ({ text, small, to, ...rest }) => {
+  const content = <span>{text}<br /><small>{small}</small></span>;
+  const inner =
+      to ? <Link to={to} className="no-decoration">{content}</Link> : content;
+  return <td {...rest}>{inner}</td>;
+};
+
+const TeamProblemCol = ({ problem: { attempts, penalty, pendings, solved } }) => {
   let status;
   let content;
   if (solved) {
@@ -88,32 +113,71 @@ const StandingsProblemCol = ({ problem: { attempts, penalty, pendings, solved } 
   );
 };
 
-const StandingsRowPin = ({ pinned, onClick }) => {
+const TeamPinCol = ({ pinned, onClick }) => {
   const className =
     'glyphicon glyphicon-pushpin' + (pinned ? ' pinned' : '');
-  return <span className={className} onClick={onClick} />
-};
-
-/*
-const TeamInfoTip = ({ team }) => {
-  const memberElements = team.members.map((profile) => (
-    <div>
-      <FixedRatioThumbnail url={profile.icon} ratio={1} />
-      {profile.name}
-    </div>
-  ));
   return (
-    <div className="team-info-tip">
-      <h3>{ team.name }</h3>
-      <h4>{ team.university }</h4>
-      <FixedRatioThumbnail url={team.photo} ratio={1 / 3} />
-      <Link to={`/team/${team.id}`}>Details</Link>
-    </div>
+    <td className="team-mark">
+      <span className={className} onClick={onClick} />
+    </td>
   );
 };
-*/
 
-class StandingsRow extends React.Component {
+const TeamRowSimple = (props) => {
+  const { status, team, pinned, onClickPin, className = '', ...rest } = props;
+  const { rank, solved, penalty } = status;
+  const { id, name, university } = team;
+  const rewrittenClassName = 'team-row ' + className;
+  return (
+    <li className={rewrittenClassName} {...rest}>
+      <table className="team-table">
+        <tbody>
+          <tr>
+            <TeamPinCol pinned={pinned} onClick={onClickPin} />
+            <TeamCol className="team-rank" text={rank} />
+            <TeamCol className="team-name" text={name} to={`/team/${id}`} />
+            <TeamCol className="team-name" text={university} to={`/team/${id}`} />
+            <TeamCol className="team-solved" text={solved} />
+            <TeamCol className="team-penalty" text={`(${penalty})`} />
+            <td />
+          </tr>
+        </tbody>
+      </table>
+    </li>
+  );
+};
+
+const TeamRowDetailed = (props) => {
+  const { status, team, pinned, onClickPin, className = '', ...rest } = props;
+  const { rank, solved, penalty, problems = [] } = status;
+  const { id, name, university } = team;
+  const rewrittenClassName = 'team-row ' + className;
+  const problemCols = [];
+  if (problems.length > 0) {
+    problems.forEach((problem) => {
+      problemCols.push(<TeamProblemCol problem={problem} />);
+    });
+  } else {
+    problemCols.push(<td />);
+  }
+  return (
+    <li className={rewrittenClassName} {...rest}>
+      <table className="team-table">
+        <tbody>
+          <tr>
+            <TeamPinCol pinned={pinned} onClick={onClickPin} />
+            <TeamCol className="team-rank" text={rank} />
+            <TeamCol className="team-name" text={name} small={university} to={`/team/${id}`} />
+            <TeamCol className="team-solved" text={solved} small={`(${penalty})`} />
+            {problemCols}
+          </tr>
+        </tbody>
+      </table>
+    </li>
+  );
+};
+
+class AnimatingTeamRow extends React.Component {
   constructor(props) {
     super(props);
     this.state = { rankHidden: false, newSolved: false };
@@ -153,67 +217,67 @@ class StandingsRow extends React.Component {
     }
   }
 
-  componentDidMount() {
-    /*
-    $(this._hover).popover({
-      container: 'body',
-      placement: 'right',
-      trigger: 'focus',
-      content: () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<TeamInfoTip team={this.props.team} />, div);
-        return div.innerHTML;
-      },
-      html: true,
+  render() {
+    const { component: Component, className = '', status, ...rest } = this.props;
+    const rewrittenClassName =
+      this.state.newSolved ? className + ' new-solved' : className;
+    const rewrittenStatus = Object.assign(
+      {}, status, this.state.rankHidden ? {rank: '...'} : {});
+    return <Component className={rewrittenClassName} status={rewrittenStatus} {...rest} />
+  }
+};
+
+class AnimatingList extends React.Component {
+  componentWillUpdate() {
+    const liList = Array.from(this._dom.children);
+    liList.forEach((li) => {
+      li.classList.remove('animating');
+      li.style.transform = undefined;
     });
-    */
+    this._lastKeyToOffsetTop = new Map();
+    liList.forEach((li, i) => {
+      const child = this.props.children[i];
+      this._lastKeyToOffsetTop.set(child.key, li.offsetTop);
+    });
   }
 
-  componentWillUnmount() {
-    this.clearTimeouts();
-    /*
-    $(this._hover).popover('destroy');
-    */
+  componentDidUpdate() {
+    const liList = Array.from(this._dom.children);
+    liList.forEach((li, i) => {
+      li.style.zIndex = 9999 - i;
+    });
+    const currentKeyToOffsetTop = new Map();
+    liList.forEach((li, i) => {
+      const child = this.props.children[i];
+      currentKeyToOffsetTop.set(child.key, li.offsetTop);
+    });
+    const rels = new Map();
+    liList.forEach((li, i) => {
+      const child = this.props.children[i];
+      const currentOffsetTop = currentKeyToOffsetTop.get(child.key);
+      const lastOffsetTop =
+        this._lastKeyToOffsetTop.has(child.key) ?
+        this._lastKeyToOffsetTop.get(child.key) :
+        currentOffsetTop;
+      const relativeOffsetTop = lastOffsetTop - currentOffsetTop;
+      rels[child.key] = relativeOffsetTop;
+      if (relativeOffsetTop != 0) {
+        li.style.transform = `translate(0, ${relativeOffsetTop}px)`;
+        setTimeout(() => {
+          li.classList.add('animating');
+          li.style.transform = 'translate(0, 0)';
+        }, 1000);
+      }
+    });
   }
 
   render() {
-    const { status, team, pinned, onClickPin, sticky, detailed } = this.props;
-    const { rank, solved, penalty, problems = [] } = status;
-    const { id, name, university } = team;
-    const cols = [];
-    if (detailed) {
-      cols.push(...problems.map(
-        (problem) => <StandingsProblemCol problem={problem} />));
-    }
-    if (cols.length == 0) {
-      cols.push(<td />);
-    }
-    const displayRank = this.state.rankHidden ? '...' : rank;
-    const { newSolved } = this.state;
+    const { children, style, ...rest } = this.props;
+    const rewrittenStyle = Object.assign({}, style, {position: 'relative'});
     return (
-      <li className={`team-row ${sticky ? 'sticky' : ''} ${newSolved ? 'new-solved' : ''}`}>
-        <table className="team-table">
-          <tbody>
-            <tr>
-              <td className="team-mark">
-                <StandingsRowPin pinned={pinned} onClick={onClickPin} />
-              </td>
-              <td className="team-rank">{displayRank}</td>
-              <td className="team-name" ref={(el) => { this._hover = el; }}>
-                <Link to={`/team/${id}`} className="no-decoration">
-                  {name}
-                  <br /><small>{university}</small>
-                </Link>
-              </td>
-              <td className="team-score">
-                {solved}
-                <br /><small>({penalty})</small>
-              </td>
-              {cols}
-            </tr>
-          </tbody>
-        </table>
-      </li>
+      <ul style={rewrittenStyle} {...rest} ref={(dom) => { this._dom = dom; }}>
+        {children}
+      </ul>
     );
   }
 };
@@ -247,43 +311,6 @@ class StandingsTable extends React.Component {
     localStorage.setItem(this.props.pinLocalStorageName, serialized);
   }
 
-  animateRows(standings, prevStandings) {
-    const rows = Array.from(this._ul.children);
-
-    rows.forEach((li, index) => {
-      // Reset row offsets first so li.offsetTop indicates the destination.
-      li.style.transform = 'translate(0, 0)';
-      li.classList.remove('animating');
-      li.style.zIndex = 9999 - index;
-    });
-
-    const indexToOffsetY = rows.map((li) => li.offsetTop);
-    const teamIdToLastIndex = {};
-    prevStandings.forEach(
-      ({ teamId }, index) => { teamIdToLastIndex[teamId] = index; });
-    const indexToLastIndex =
-      standings.map(({ teamId }) => teamIdToLastIndex[teamId]);
-
-    // Schedule animation.
-    rows.forEach((li, index) => {
-      const lastIndex = indexToLastIndex[index];
-      const relativeOffsetY =
-        lastIndex === undefined ?
-        0 : indexToOffsetY[lastIndex] - indexToOffsetY[index];
-      if (relativeOffsetY != 0) {
-        li.style.transform = `translate(0, ${relativeOffsetY}px)`;
-        setTimeout(() => {
-          li.classList.add('animating');
-          li.style.transform = 'translate(0, 0)';
-        }, 1000);
-      }
-    });
-  }
-
-  componentDidUpdate(prevProps) {
-    this.animateRows(this.props.standings, prevProps.standings);
-  }
-
   handleClickPin(id) {
     const newPinnedTeamIdSet = new Set(this.state.pinnedTeamIdSet);
     if (this.state.pinnedTeamIdSet.has(id)) {
@@ -296,17 +323,18 @@ class StandingsTable extends React.Component {
 
   render() {
     const { standings, teamsMap, problems, detailed } = this.props;
+    const TeamRow = detailed ? TeamRowDetailed : TeamRowSimple;
+    const LegendRow = detailed ? LegendRowDetailed : LegendRowSimple;
     const normalRows = standings.map((status) => {
       const team = teamsMap[status.teamId] || DEFAULT_TEAM;
       return (
-        <StandingsRow
-          key={`normal ${status.teamId}`}
+        <AnimatingTeamRow
+          component={TeamRow}
+          key={status.teamId}
           status={status}
           team={team}
-          detailed={detailed}
           pinned={this.state.pinnedTeamIdSet.has(status.teamId)}
           onClickPin={() => this.handleClickPin(status.teamId)}
-          sticky={false}
         />
       );
     });
@@ -315,29 +343,27 @@ class StandingsTable extends React.Component {
     const stickyRows = pinnedStandings.map((status) => {
       const team = teamsMap[status.teamId] || DEFAULT_TEAM;
       return (
-        <StandingsRow
-          key={`sticky ${status.teamId}`}
+        <AnimatingTeamRow
+          component={TeamRow}
           status={status}
           team={team}
-          detailed={detailed}
           pinned={true}
           onClickPin={() => this.handleClickPin(status.teamId)}
-          sticky={true}
+          className="sticky"
         />
       );
     });
     return (
       <div className="standings">
         <ul className="list-unstyled">
-          <StandingsLegendRow problems={problems} detailed={detailed} />
+          <LegendRow problems={problems} />
         </ul>
         <ul className="list-unstyled">
           {stickyRows}
         </ul>
-        <ul className="list-unstyled"
-            ref={(ul) => { this._ul = ul; }}>
+        <AnimatingList className="list-unstyled">
           {normalRows}
-        </ul>
+        </AnimatingList>
       </div>
     );
   }
