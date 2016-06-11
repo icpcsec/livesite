4daadf0734e91e192bab122abe931e60ce4c157f
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2016-06-11 14:37:04 +0900

    Remove title from front page

--- app/client/js/components/FrontContent.js
+++ app/client/js/components/FrontContent.js
@@ -1,12 +1,7 @@
 import React from 'react';
 
 const FrontContent = ({ contest }) => (
-  <div>
-    <h1 className="page-header">
-      { contest.title }
-    </h1>
-    <div dangerouslySetInnerHTML={{ __html: contest.frontPageHtml }} />
-  </div>
+  <div dangerouslySetInnerHTML={{ __html: contest.frontPageHtml }} />
 );
 
 export default FrontContent;
