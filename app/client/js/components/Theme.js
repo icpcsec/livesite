5574a35deeabc673e318e3f047157987c4b3d90c
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2016-10-08 12:45:21 +0900

    Tweak dark theme

--- app/client/js/components/Theme.js
+++ app/client/js/components/Theme.js
@@ -3,7 +3,9 @@ import React from 'react';
 const DARK_STYLES = (
   'body { color: #fff !important; background-color: #303030 !important; }' +
   '.navbar { background-color: #212121 !important; }' +
-  '.panel { background-color: #424242 !important; }'
+  '.panel { background-color: #424242 !important; }' +
+  '.form-control { color: #e0e0e0 !important; }' +
+  'textarea { background-color: #303030 !important; }'
 );
 
 const Theme = ({ settings }) => {
