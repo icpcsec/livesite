f257da39cb031e3f005433c6c8ee17f0f165e879
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2017-06-17 10:03:53 +0900

    update siteconfig

--- app/client/js/siteconfig.js
+++ app/client/js/siteconfig.js
@@ -1,12 +1,12 @@
-export const JA = false;
+export const JA = true;
 
-export const ENABLE_COUNTRY = true;
-export const ENABLE_PREFECTURE = false;
+export const ENABLE_COUNTRY = false;
+export const ENABLE_PREFECTURE = true;
 export const ENABLE_PHOTO_UPLOAD = false;
-export const ENABLE_ICON = true;
+export const ENABLE_ICON = false;
 export const USE_ICONS_AS_PHOTO = false;
 
 export const COMMENT_LIMIT_CHARS = 140;
 export const PHOTO_ASPECT_RATIO = 9 / 16;
 
-export const STREAMING_NICOLIVE_ID = 'ch2525484';
+export const STREAMING_NICOLIVE_ID = null;
