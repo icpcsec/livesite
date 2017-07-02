2646e7c00f4cbaea277f2269c7907a79f3132a1d
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2017-07-02 14:06:06 +0900

    Production configs1

--- app/bin/run-prod.sh
+++ app/bin/run-prod.sh
@@ -3,8 +3,6 @@
 exec uwsgi \
   --module=livesite.wsgi_main:app \
   --lazy-app \
-  --logto=/dev/null \
-  --logger=syslog:livesite-server \
   --http-socket=:8080 \
   --listen=128 \
   --master \
