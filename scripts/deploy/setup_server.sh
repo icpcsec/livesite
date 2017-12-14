7e4bf9bb7d385be8ec7990ba083d0f369488eed8
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2017-12-14 18:19:25 +0900

    Rebase for Debian stretch.

--- scripts/deploy/setup_server.sh
+++ scripts/deploy/setup_server.sh
@@ -12,18 +12,19 @@ third_party/google-cloud-sdk/ensure_download.sh
 third_party/google-cloud-sdk/bin/gcloud compute ssh "$@" --command='
 set -ex
 
-if ! dpkg -l docker-engine > /dev/null 2>&1; then
+if ! dpkg -l docker-ce > /dev/null 2>&1; then
   sudo apt-get update
-  sudo apt-get install -y apt-transport-https ca-certificates < /dev/null
-  sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-  echo "deb https://apt.dockerproject.org/repo debian-jessie main" | sudo tee /etc/apt/sources.list.d/docker.list
+  sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common < /dev/null
+  curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+  sudo apt-key fingerprint 0EBFCD88
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
   sudo apt-get update
-  sudo apt-get install -y docker-engine < /dev/null
+  sudo apt-get install -y docker-ce < /dev/null
   sudo gpasswd -a $USER docker
 fi
 
 if [[ ! -f /usr/local/bin/docker-compose ]]; then
-  curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` > docker-compose
+  curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` > docker-compose
   chmod +x docker-compose
   sudo cp docker-compose /usr/local/bin/
   rm docker-compose
