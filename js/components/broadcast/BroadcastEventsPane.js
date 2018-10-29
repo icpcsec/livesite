9f1a173f0e1f8167cafb494a8b133b4edaf0c7e6
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2018-10-29 09:40:53 +0900

    Animate event table.

--- js/components/broadcast/BroadcastEventsPane.js
+++ js/components/broadcast/BroadcastEventsPane.js
@@ -1,18 +1,16 @@
+import deepEqual from 'deep-equal';
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
-const MAX_ROWS = 10;
+const EVENT_TIMEOUT_SECONDS = 30;
 
 const EventRow = ({type, team, problem, oldRank, newRank}) => {
-  const mark = type === 'accept' ? '✔' : '✘';
   return (
     <div className="card broadcast-card">
       <div className={`card-body card-event-${type} text-ellipsis`}>
         <span className="mark" style={{ marginRight: '4px' }}>
-          <span style={{ display: 'inline-block', width: '16px', textAlign: 'center' }}>
+          <span style={{ display: 'inline-block', width: '12px', textAlign: 'center' }}>
             {problem.label}
-          </span>
-          <span style={{ display: 'inline-block', width: '16px', textAlign: 'center' }}>
-            {mark}
           </span>
         </span>
         {team.universityShort} {team.name}
@@ -21,26 +19,68 @@ const EventRow = ({type, team, problem, oldRank, newRank}) => {
   );
 };
 
-const BroadcastEventsPane = ({ events, teams, problems }) => {
-  const now = new Date().getTime() / 1000;
-  const rows = [];
-  for (let eventIndex = events.length - 1; eventIndex >= 0 && rows.length < MAX_ROWS; --eventIndex) {
-    const event = events[eventIndex];
-    const {eventId, type, time, teamId, problemIndex, oldRank, newRank} = event;
-    if (now - time >= 30) {
-      continue;
+class EventTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: [],
+    };
+  }
+
+  componentDidUpdate() {
+    this.updateState_();
+  }
+
+  componentDidMount() {
+    this.timer_ = setInterval(() => this.updateState_(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer_);
+  }
+
+  updateState_() {
+    const newEvents = [];
+    const now = new Date().getTime() / 1000;
+    for (const e of this.props.events) {
+      if (now - e.time <= EVENT_TIMEOUT_SECONDS) {
+        newEvents.push(e);
+      }
     }
-    const team = teams[teamId];
-    const problem = problems[problemIndex];
-    rows.push(
-      <EventRow key={eventId} type={type} team={team} problem={problem} oldRank={oldRank} newRank={newRank} />
+
+    if (!deepEqual(this.state.events, newEvents)) {
+      this.setState({
+        events: newEvents,
+      });
+    }
+  }
+
+  render() {
+    const {teams, problems} = this.props;
+    const {events} = this.state;
+    const rows = [];
+    for (const event of events) {
+      const {eventId, type, teamId, problemIndex, oldRank, newRank} = event;
+      const team = teams[teamId];
+      const problem = problems[problemIndex];
+      rows.push(
+          <EventRow key={eventId} type={type} team={team} problem={problem} oldRank={oldRank} newRank={newRank} />
+      );
+    }
+    return (
+      <div style={{display: 'flex', flexDirection: 'column-reverse', width: '100%', height: '100%' }}>
+        <ReactCSSTransitionGroup transitionName="event-animation" transitionEnterTimeout={500} transitionLeaveTimeout={300}>
+          {rows}
+        </ReactCSSTransitionGroup>
+      </div>
     );
   }
+}
+
+const BroadcastEventsPane = ({ events, teams, problems }) => {
   return (
       <div style={{position: 'absolute', top: '20px', bottom: '20px', left: '20px', width: '240px' }}>
-        <div style={{display: 'flex', flexDirection: 'column-reverse', width: '100%', height: '100%' }}>
-          {rows}
-        </div>
+        <EventTable events={events} teams={teams} problems={problems} />
       </div>
   );
 };
