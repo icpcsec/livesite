b71d34be4e1f864946feda8cb6bee3b5b6eddd61
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2019-11-16 17:34:38 +0900

    Improve make_reveal.

--- scripts/make_reveal.py
+++ scripts/make_reveal.py
@@ -32,6 +32,11 @@ gflags.MarkFlagAsRequired('final_standings')
 gflags.MarkFlagAsRequired('output_json')
 
 
+def rank_key(entry):
+    times = list(reversed(sorted(p['penalty'] for p in entry['problems'] if p['solved'])))
+    return tuple([-entry['solved'], entry['penalty']] + times)
+
+
 def recompute_entries(entries):
     for entry in entries:
         entry['solved'] = len([p for p in entry['problems'] if p['solved']])
@@ -39,9 +44,12 @@ def recompute_entries(entries):
             p['penalty'] + 20 * (p['attempts'] - 1) for p in entry['problems']
             if p['solved']
         ])
-    entries.sort(key=lambda entry: (-entry['solved'], entry['penalty']))
+    entries.sort(key=rank_key)
+    rank = -1
     for i, entry in enumerate(entries):
-        entry['rank'] = i + 1
+        if i == 0 or rank_key(entry) != rank_key(entries[i - 1]):
+            rank = i + 1
+        entry['rank'] = rank
 
 
 def check_entries_equals(entries1, entries2):
@@ -50,7 +58,9 @@ def check_entries_equals(entries1, entries2):
     for i, (entry1, entry2) in enumerate(zip(entries1, entries2)):
         if entry1 != entry2:
             print 'ERROR: standings mismatch at rank %d' % (i + 1)
+            print 'got:'
             print entry1
+            print 'want:'
             print entry2
 
 
@@ -60,21 +70,32 @@ def main(unused_argv):
     with open(FLAGS.final_standings) as f:
         standings_end = json.load(f)
 
-    assert standings_start['problems'] == standings_end['problems']
-    all_problems = standings_start['problems']
+    assert len(standings_start['problems']) == len(standings_end['problems'])
+    all_problems = standings_end['problems']
 
     entries_start = standings_start['entries']
     entries_end = standings_end['entries']
+
+    assert len(entries_start) == len(entries_end)
 
     for entry in entries_start:
         entry['revealState'] = 'pending'
     for entry in entries_end:
         entry['revealState'] = 'finalized'
 
+    for entry in entries_end:
+        for problem in entry['problems']:
+            assert problem['pendings'] == 0, 'Team %s has pending results in the end' % entry['teamId']
+
     final_map = {}
     for entry in entries_end:
         for i, problem in enumerate(entry['problems']):
             final_map[(entry['teamId'], i)] = problem
+
+    for entry in entries_start:
+        for i, problem_start in enumerate(entry['problems']):
+            problem_end = final_map[(entry['teamId'], i)]
+            assert problem_end['attempts'] <= problem_start['attempts'] + problem_start['pendings'], 'Team %s problem %d submissions increased' % (entry['teamId'], i)
 
     def reveal_step():
         for entry in reversed(entries):
@@ -94,11 +115,15 @@ def main(unused_argv):
     check_entries_equals(entries, entries_start)
 
     entries_list = [copy.deepcopy(entries)]
+    step = 0
     while reveal_step():
         recompute_entries(entries)
         entries_list.append(copy.deepcopy(entries))
         sys.stdout.write('.')
         sys.stdout.flush()
+        step += 1
+        if step >= 1000:
+            assert False, entries
     sys.stdout.write('\n')
     sys.stdout.flush()
 
